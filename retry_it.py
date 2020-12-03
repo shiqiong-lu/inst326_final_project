b89033e7dcfdd7b09bfd0417d8fa3515439db57c
ahoang13-git <73260565+ahoang13-git@users.noreply.github.com>
2020-12-03 14:45:26 -0500

    Create retry_it.py

---
+++ retry_it.py
@@ -0,0 +1,11 @@
+def retry_it(): # coded by Ann Hoang
+    '''Yes or no option to allow the user to retake the quiz
+    
+    Args:
+        answer (int): yes or no to retaking quiz
+    
+    Returns:
+        answer (int): yes or no to retaking quiz
+    '''
+    re_answer = int(input('Retake quiz: (1) yes or (2) no? '))
+    return re_answer
