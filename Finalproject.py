42c9dcc03c57f84f4c1531cced9b1a76d783a7cd
Hung95red <74378589+Hung95red@users.noreply.github.com>
2020-12-03 14:45:04 -0500

    Add files via upload

---
+++ Finalproject.py
@@ -0,0 +1,10 @@
+"""This script is allowing the user create their own quiz
+"""
+import csv
+class Createquiz:
+   
+    def main(user):
+            bs = different_class()
+        for quiz in user:
+            bs.create_quiz(quiz) 
+   
