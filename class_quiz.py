b386a7791a89b67521452ca87123911adc55677e
ahoang13-git <73260565+ahoang13-git@users.noreply.github.com>
2020-12-07 17:30:02 -0500

    Update class_quiz.py

--- class_quiz.py
+++ class_quiz.py
@@ -15,7 +15,6 @@ def presentquestions_getchoices():
         field (int): field choice
         cube_texture (int): cube texture choice
         cube_color (int): cube color choice
-        cube_other (int): possible other cube choices
         ladder_length (int): ladder length choice
         ladder_dist (int): ladder distance choice
         ladder_dist_cube (int): ladder distance to cube choice
@@ -31,7 +30,6 @@ def presentquestions_getchoices():
         field (int): field choice
         cube_texture (int): cube texture choice
         cube_color (int): cube color choice
-        cube_other (int): possible other cube choices
         ladder_length (int): ladder length choice
         ladder_dist (int): ladder distance choice
         ladder_dist_cube (int): ladder distance to cube choice
@@ -45,27 +43,165 @@ def presentquestions_getchoices():
     '''
     print("Please enter the corresponding number for each answer choice")
     
-    field = int(input('Think of an open field. Is it (1) dry and dead, (2) grassy and healthy, or (3) well-trimmed? '))
-    cube_texture = int(input('Think of a cube. Is it (1) smooth, (2) rough, or (3) bumpy/spiky? '))
-    cube_color = int(input('Is the cube (1) red, (2) yellow, (3) blue, (4) violet, (5) grey, (6) black, or (7) white? '))
-    #cube_other = int(input('Is the cube (1) transparent, (2) made out of water/ice, (3) hollow, or (4) made out of metal/rock? '))
-    ladder_length = int(input('Think of a ladder. Is it (1) short or (2) long? '))
-    ladder_dist = int(input('Is the ladder (1) near or (2) far? '))
-    ladder_dist_cube = int(input('Is the ladder (1) near the cube or (2) far from the cube? '))
-    ladder_mat = int(input('Is the ladder made of (1) strong material or (2) weak material? '))
-    horse_act = int(input('Think of a horse. What is it doing? Is it (1) playing, (2) running, or (3) sleeping/grazing? '))
-    horse_col = int(input('What color is the horse? Is it (1) brown, (2) black, or (3) white? '))
-    flowers = int(input('Think of flowers. Are there (1) few or are they (2) everywhere? '))
-    weather = int(input('Think of what the weather in the field is like. Is there (1) rain, (2) fog, (3) wind, or (4) sun? '))
-    storm_inten = int(input('Think of a storm. Is it (1) mild or (2) strong? '))
-    storm_loc = int(input('Is the storm (1) in the background or (2) right above the cube? '))
+    while True:
+        try:
+            field = int(input('Think of an open field. Is it (1) dry and dead, (2) grassy and healthy, or (3) well-trimmed? '))
+            if field in range(1, 3):
+                break
+            else:
+                print('Please enter an integer from 1-3.')
+                continue
+        except ValueError:
+            print('Please enter an integer.')
+            continue
+        
+    while True:
+        try:
+            cube_texture = int(input('Think of a cube. Is it (1) smooth, (2) rough, or (3) bumpy/spiky? '))
+            if cube_texture in range(1, 3):
+                break
+            else:
+                print('Please enter an integer from 1-3.')
+                continue
+        except ValueError:
+            print('Please enter an integer.')
+            continue
     
-    return (field,cube_texture,
-            cube_color,
-            ladder_length,ladder_dist,
-            ladder_dist_cube,ladder_mat,
-            horse_act,horse_col,flowers,weather,
-            storm_inten,storm_loc)
+    while True:
+        try:
+            cube_color = int(input('Is the cube (1) red, (2) yellow, (3) blue, (4) violet, (5) grey, (6) black, or (7) white? '))
+            if cube_color in range(1, 7):
+                break
+            else:
+                print('Please enter an integer from 1-7.')
+                continue
+        except ValueError:
+            print('Please enter an integer.')
+            continue
+    
+    while True:
+        try:
+            ladder_length = int(input('Think of a ladder. Is it (1) short or (2) long? '))
+            if ladder_length in range(1, 2):
+                break
+            else:
+                print('Please enter 1 or 2.')
+                continue
+        except ValueError:
+            print('Please enter an integer.')
+            continue
+    
+    while True:
+        try:
+            ladder_dist = int(input('Is the ladder (1) near or (2) far? '))
+            if ladder_dist in range(1, 2):
+                break
+            else:
+                print('Please enter 1 or 2.')
+                continue
+        except ValueError:
+            print('Please enter an integer.')
+            continue
+    
+    while True:
+        try:
+            ladder_dist_cube = int(input('Is the ladder (1) near the cube or (2) far from the cube? '))
+            if ladder_dist_cube in range(1, 2):
+                break
+            else:
+                print('Please enter 1 or 2.')
+                continue
+        except ValueError:
+            print('Please enter an integer.')
+            continue
+    
+    while True:
+        try:
+            ladder_mat = int(input('Is the ladder made of (1) strong material or (2) weak material? '))
+            if ladder_mat in range(1, 2):
+                break
+            else:
+                print('Please enter 1 or 2.')
+                continue
+        except ValueError:
+            print('Please enter an integer.')
+            continue
+    
+    while True:
+        try:
+            horse_act = int(input('Think of a horse. What is it doing? Is it (1) playing, (2) running, or (3) sleeping/grazing? '))
+            if horse_act in range(1, 3):
+                break
+            else:
+                print('Please enter an integer from 1-3.')
+                continue
+        except ValueError:
+            print('Please enter an integer.')
+            continue
+    
+    while True:
+        try:
+            horse_col = int(input('What color is the horse? Is it (1) brown, (2) black, or (3) white? '))
+            if horse_col in range(1, 3):
+                break
+            else:
+                print('Please enter an integer from 1-3.')
+                continue
+        except ValueError:
+            print('Please enter an integer.')
+            continue
+    
+    while True:
+        try:
+            flowers = int(input('Think of flowers. Are there (1) few or are they (2) everywhere? '))
+            if flowers in range(1, 2):
+                break
+            else:
+                print('Please enter 1 or 2.')
+                continue
+        except ValueError:
+            print('Please enter an integer.')
+            continue
+    
+    while True:
+        try:
+            weather = int(input('Think of what the weather in the field is like. Is there (1) rain, (2) fog, (3) wind, or (4) sun? '))
+            if weather in range(1, 4):
+                break
+            else:
+                print('Please enter an integer from 1-4.')
+                continue
+        except ValueError:
+            print('Please enter an integer.')
+            continue
+    
+    while True:
+        try:
+            storm_inten = int(input('Think of a storm. Is it (1) mild or (2) strong? '))
+            if storm_inten in range(1, 2):
+                break
+            else:
+                print('Please enter 1 or 2.')
+                continue
+        except ValueError:
+            print('Please enter an integer.')
+            continue
+    
+    while True:
+        try:
+            storm_loc = int(input('Is the storm (1) in the background or (2) right above the cube? '))
+            if storm_loc in range(1, 2):
+                break
+            else:
+                print('Please enter 1 or 2.')
+                continue
+        except ValueError:
+            print('Please enter an integer.')
+            continue
+    
+    return (field, cube_texture, cube_color, ladder_length, ladder_dist, 
+            ladder_dist_cube, ladder_mat, horse_act, horse_col, flowers, 
+            weather, storm_inten, storm_loc)
 
 class Quiz_results:
     '''Class will calculate and display results
