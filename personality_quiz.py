418bd6abfe7ca7b6b331fc80510f18c74c778c5a
ahoang13-git <73260565+ahoang13-git@users.noreply.github.com>
2020-12-06 22:57:36 -0500

    Update personality_quiz.py

--- personality_quiz.py
+++ personality_quiz.py
@@ -1125,10 +1125,10 @@ def retry_it():
     '''Yes or no option to allow the user to retake the quiz
     
     Args:
-        answer (int): yes or no to retaking quiz
+        re_answer (int): yes or no to retaking quiz
     
     Returns:
-        answer (int): yes or no to retaking quiz
+        exit (function): no to retaking quiz
     '''
     re_answer = int(input('Retake quiz: (1) yes or (2) no? '))
     if re_answer == 1:
