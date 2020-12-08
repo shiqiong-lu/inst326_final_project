644098ba7d92b3f7839e04cff638ec20b48d7578
ahoang13-git <73260565+ahoang13-git@users.noreply.github.com>
2020-12-07 21:48:23 -0500

    Update personality_quiz.py

--- personality_quiz.py
+++ personality_quiz.py
@@ -1278,7 +1278,7 @@ def retry_it():
                 print('Please enter 1 or 2.')
                 continue
         except ValueError:
-            print('Please enter an integer.')
+            print('Please enter 1 or 2.')
             continue
     
 if __name__=="__main__":
