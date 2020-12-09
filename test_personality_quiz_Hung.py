69389c4e9ef8a283928193e125f937659ff95264
Hung95red <74378589+Hung95red@users.noreply.github.com>
2020-12-09 16:06:01 -0500

    Add files via upload

---
+++ test_personality_quiz_Hung.py
@@ -0,0 +1,11 @@
+import personality_quiz
+
+"""
+Professor said that:
+Main tip for assert testing is to compare a sample set of responses 
+to the personality type the program SHOULD give you.
+"""
+personality_quiz.Quiz_results(1)
+
+def_personality_quiz():
+    self
